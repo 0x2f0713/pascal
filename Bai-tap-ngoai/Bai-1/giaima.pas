1a67c7b2d54c3c9aacb0f629f1fe70d73e7825fa
0x2f0713 <namhaiha0308@gmail.com>
2018-04-21 18:47:35 +0700

    Update algorithm decrypt hash!

--- Bai-tap-ngoai/Bai-1/giaima.pas
+++ Bai-tap-ngoai/Bai-1/giaima.pas
@@ -1,73 +1,75 @@
+(* 
+   ___       ___   __  ___ ______ __ ____  
+  / _ \     |__ \ / _|/ _ \____  /_ |___ \ 
+ | | | |_  __  ) | |_| | | |  / / | | __) |
+ | | | \ \/ / / /|  _| | | | / /  | ||__ < 
+ | |_| |>  < / /_| | | |_| |/ /   | |___) |
+  \___//_/\_\____|_|  \___//_/    |_|____/                                           
+*)
+
+
 var
-    xau,xau_moi,check: string;
+    xau: string;
     i,count:integer;
     fi,fo:text;
+(* Hàm giải mã *)
+function decode(xau:string): string;
+var
+    xau_moi,check: string;
+    i,count:integer;
 begin
-	assign(fi,'decrypt.inp');
-	assign(fo,'decrypt.out');
-	reset(fi); rewrite(fo);
-	readln(fi,xau);
-	count:=0;
+    count:=0;
     xau_moi:='';
     for i:=length(xau) downto 1 do
     begin
-    	    if count <> 0 then
+            if count <> 0 then
     begin
-    	count:=count-1;
-    	continue;
+        count:=count-1;
+        continue;
     end;
         case xau[i] of
             'a': begin
-                check:= xau[i-2] + xau[i-1] + xau[i];
-                case check of
-                    'abc':begin
-                    	xau_moi:=xau_moi + '6';
-                    	count:=2;
-                    end;
-                    'aac':begin
-                    	xau_moi:=xau_moi + '8';
-                    	count:=2;
-                    end; 
-                    else begin
-                    	xau_moi:= xau_moi+'1';
-                    end;
-                end;
-                    
+                xau_moi:=xau_moi+'1';
+                continue;
             end;
             'b': begin
-                check:= xau[i-2] + xau[i-1] + xau[i];
-                case check of
-                    'bbc': begin
-                    	xau_moi:=xau_moi+'4';
-                    	count:=2;
-                    end;
-                    'bac': begin
-                    	xau_moi:=xau_moi+'7';
-                    	count:=2;
-                    end;
-                    else begin
-                    	xau_moi:=xau_moi+'2';
-                    end;
-                end;
+                xau_moi:=xau_moi+'2';
             end;
             'c': begin
-                if xau[i+1] = 'c' then begin
-                	xau_moi:=xau_moi+'3';
-                	count:=1;
+                if xau[i-1] = 'c' then begin
+                    xau_moi:=xau_moi+'3';
+                    count:=1;
                 end
                 else
                 begin
                     check:= xau[i-2] + xau[i-1] + xau[i];
                     case check of
                         'cbc':begin
-                        	xau_moi:=xau_moi+'5';
-                        	count:=2;
+                            xau_moi:=xau_moi+'5';
+                            count:=2;
                         end;
                         'cac':begin
-                        	xau_moi:=xau_moi+'9';
-                        	count:=2;
+                            xau_moi:=xau_moi+'9';
+                            count:=2;
                         end;
+                        'bbc': begin
+                            xau_moi:=xau_moi+'4';
+                            count:=2;
+                        end;
+                        'bac': begin
+                            xau_moi:=xau_moi+'7';
+                            count:=2;
+                        end;
+                        'abc':begin
+                            xau_moi:=xau_moi + '6';
+                            count:=2;
+                        end;
+                        'aac':begin
+                            xau_moi:=xau_moi + '8';
+                            count:=2;
+                        end; 
                     end;
+
                 end;
             end;           
         end;
@@ -75,8 +77,52 @@ begin
         check:='';
         for i := length(xau_moi) downto 1 do
         begin
-        	check:=check+xau_moi[i];
+            check:=check+xau_moi[i];
         end;
-        writeln(fo,check);
-        close(fo);
+        decode:=check;
+end;
+(* Hàm mã hóa để mình test thử chương trình *)
+function encode(so:Integer): string;
+var
+    chuoi,ketqua: string;
+    i:Integer;
+begin
+    str(so,chuoi);
+    ketqua:='';
+    for i:=0 to length(chuoi) do
+    begin
+        case chuoi[i] of
+            '0': ketqua:=ketqua+'';
+            '1': ketqua:=ketqua+'a';
+            '2': ketqua:=ketqua+'b';
+            '3': ketqua:=ketqua+'cc';
+            '4': ketqua:=ketqua+'bbc';
+            '5': ketqua:=ketqua+'cbc';
+            '6': ketqua:=ketqua+'abc';
+            '7': ketqua:=ketqua+'bac';
+            '8': ketqua:=ketqua+'aac';
+            '9': ketqua:=ketqua+'cac';
+        end;
+    end;
+    encode:=ketqua;
+end;
+(* Chương trình chính *)
+begin
+	assign(fi,'decrypt.inp');
+	assign(fo,'decrypt.out');
+	reset(fi); rewrite(fo);
+	readln(fi,xau);
+
+    (* Phần chương trình chạy thử *)
+    // for i:=1 to 100 do
+    // begin
+    //     str(i,xau);
+    //     if decode(encode(i)) = xau then writeln('TRUE')
+    //     else begin
+    //         writeln(i,' ',encode(i),' ','FALSE');
+    //     end;
+    // end;
+
+    writeln(fo,decode(xau));
+    close(fo);
 end.
